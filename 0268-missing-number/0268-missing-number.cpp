c1038d1b28901eaddd5596be36d64415d2f2ced4
trqvu21 <tqv4560@gmail.com>
2026-04-07 10:12:37 +0700

    Time: 1 ms (21.86%), Space: 21.6 MB (99.78%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/missing-number/description/?envType=problem-list-v2&envId=w5srjw5v
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if (nums[0] != 0){
+            return 0;
+        }
+        for(int i=0; i<nums.size()-1;i++)
+        if(nums[i] != nums[i+1]-1){
+            return nums[i] + 1;
+        }
+        return nums.size();
+    }
+};
